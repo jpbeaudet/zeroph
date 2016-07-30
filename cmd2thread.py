c2cb99458c077e067a0c8dfbf73b90f7e9956cf6
jpbeaudet <jpbeaudet84@gmail.com>
2016-07-30 16:33:01 -0400

    test with classic threading approach

--- cmd2thread.py
+++ cmd2thread.py
@@ -18,6 +18,21 @@ import subprocess
 from subprocess import Popen, PIPE
 import threading
 
+class cmdThread (threading.Thread):
+    def __init__(self, threadID, name, cmd):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.cmd = cmd
+    def run(self):
+        print "Starting " + self.name
+        # Get lock to synchronize threads
+        threadLock.acquire()
+        result = cmd(self.cmd)
+        # Free lock to release next thread
+        threadLock.release()
+        return result
+
 class CmdToThread(object):
     
     def __init__(self, verbose):
@@ -41,8 +56,15 @@ class CmdToThread(object):
             if isinstance(msg, str):
                 # Create two threads as follows
                 try:
-                    response = thread.start_new_thread(self.cmd, (msg, ))
-                    socket.send(str(response))
+                    # Create a new thread
+                    thread1 = myThread(1, str(msg.plit(",")[1], msg)
+
+                    # Start a new Thread
+                    thread1.start()
+
+                    print "Exiting Main Thread"
+                    #response = thread.start_new_thread(self.cmd, (msg, ))
+                    socket.send(str(thread1.get()))
                 except:
                     print(str(timenow())+' CmdToThread() WARNING | Error: unable to start thread ')
                     socket.send(str(timenow())+' CmdToThread() WARNING | Error: unable to start thread ')
@@ -64,10 +86,6 @@ class CmdToThread(object):
         socket = context.socket(zmq.REQ)
         socket.connect('tcp://127.0.0.1:5555')
         
-        #cmd = sum([_type, _file], _cmd.split(" "))
-        #cmd =[]
-        #cmd.append(_type)
-        #cmd.append(_file)
         s =","
         cmd = s.join((_type,_file))
         for arg in _cmd:
@@ -75,42 +93,40 @@ class CmdToThread(object):
                 cmd= cmd+","
                 if self.verbose:
                     print(str(timenow())+' CmdToThread() INFO | arg in _cmd: ' + str(arg)) 
-                #cmd.append(arg)
+
                 cmd = cmd + arg.replace("[","").replace("]","") 
-        #cmd= cmd+ " ]"
-        #cmd = sum(cmd, _cmd)
+
         if self.verbose:
             print(str(timenow())+' CmdToThread() INFO | cmd sent to server: ' + str(cmd))
-        #data = json.dumps(cmd)
         socket.send(cmd)
         msg = socket.recv()
         if self.verbose:
             print(str(timenow())+' CmdToThread() INFO | server returned response: ' + str(msg))
         
         
-    def cmd(self, cmd):
-        """
-        Run the actual command in thread
+def cmd(cmd):
+    """
+    Run the actual command in thread
         
-        @params: {str} cmd: the command to run
-        @rtype: {} return value
+    @params: {str} cmd: the command to run
+    @rtype: {} return value
         
-        """
+    """
 
 
+    if self.verbose:
+        print(str(timenow())+' CmdToThread() INFO | Thread started for : ' + str(cmd))
+    query= cmd.split(",")
+    if self.verbose:
+        print(str(timenow())+' CmdToThread() INFO | query : ' + str(query))
+    process = Popen(query, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    if stderr:
+        print(str(timenow())+' CmdToThread() WARNING | cmd returned error: ' + str(stderr))
+    else:
         if self.verbose:
-            print(str(timenow())+' CmdToThread() INFO | Thread started for : ' + str(cmd))
-        query= cmd.split(",")
-        if self.verbose:
-            print(str(timenow())+' CmdToThread() INFO | query : ' + str(query))
-        process = Popen(query, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
-        if stderr:
-            print(str(timenow())+' CmdToThread() WARNING | cmd returned error: ' + str(stderr))
-        else:
-            if self.verbose:
-                print(str(timenow())+' CmdToThread() INFO | cmd returned stdout: ' + str(stdout))
-            return stdout
+            print(str(timenow())+' CmdToThread() INFO | cmd returned stdout: ' + str(stdout))
+        return stdout
             
 def timenow():
     return datetime.datetime.now().time()
