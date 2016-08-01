a8630384860d2193e3e4f31d141e12266884939a
jpbeaudet <jpbeaudet84@gmail.com>
2016-08-01 18:45:23 -0400

    testing an error

--- zeroph.py
+++ zeroph.py
@@ -176,7 +176,7 @@ class ZeroPh(object):
                             CMD.append(command)
                     else:
                         CMD.append(cmds[1])
-                    CMD=["Test2"]
+                    CMD=["Test2","Test3"]
                     q1 = enthread(self.wait_cascade, (CMD))
                     #self.wait_cascade(CMD)
                     continue
