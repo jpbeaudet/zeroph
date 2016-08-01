e7c89c6d8fca61150d60233da23f9248e6178b3b
jpbeaudet <jpbeaudet84@gmail.com>
2016-08-01 18:00:01 -0400

    start on the group process at init and integrate necessary methods

--- zeroph.py
+++ zeroph.py
@@ -161,7 +161,7 @@ class ZeroPh(object):
         
         """
         if len(commands) >0:
-            for cmds in commands
+            for cmds in commands:
                 if is_number(cmds[0]):
                     if self.verbose:
                         print(str(timenow())+' ZeroPh() INFO | Init process: '+str(cmds[1])': waiting ' + str(cmds[0])+' seconds')
